cd6606fb42ae926c06e4cdd18babccaa89afbb1e
Rodolfo <rodolfo.cassaro@auaha.net>
2023-08-09 07:57:58 -0300

    adição do SVG por outro método para que aparecça no vercel

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 
 import styles from "./Header.module.css"
+import Logo from "../../../public/Logo.svg"
 
 export const Header = () =>{
 return(
         <header className={styles.headerContainer}>
-            <img src="../../../public/Logo.svg" alt="Logotipo ToDoList RocketSeat"/>
+            <img src={Logo} alt="Logotipo ToDoList RocketSeat"/>
         </header>
 )
 }
